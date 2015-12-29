427afacde948da01fddb2df0d14e09ee2a4b7e94
parkjoon <parkjoon@umich.edu>
2015-12-29 16:36:27 -0500

    updates

--- src/main/java/io/github/parkjoon/SpringSocialDemo/config/ThymeleafContext.java
+++ src/main/java/io/github/parkjoon/SpringSocialDemo/config/ThymeleafContext.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.ViewResolver;
 import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafView;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -18,50 +17,57 @@ import java.util.Set;
 
 @Configuration
 public class ThymeleafContext {
+
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine engine = new SpringTemplateEngine();
+
         Set<IDialect> dialects = new HashSet<IDialect>();
         dialects.add(new SpringSecurityDialect());
         dialects.add(new LayoutDialect());
         engine.setAdditionalDialects(dialects);
+
         LinkedHashSet<ITemplateResolver> templateResolvers = new LinkedHashSet<ITemplateResolver>(2);
         templateResolvers.add(templateResolverServlet());
         templateResolvers.add(layoutTemplateResolverServlet());
         engine.setTemplateResolvers(templateResolvers);
+
         return engine;
     }
 
     @Bean
     public ServletContextTemplateResolver layoutTemplateResolverServlet() {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-        templateResolver.setPrefix("/WEB-INF/layout/");
+        templateResolver.setPrefix("/templates/layout/");
         templateResolver.setSuffix("");
         templateResolver.setTemplateMode("LEGACYHTML5");
         templateResolver.setOrder(1);
         templateResolver.setCacheable(false);
+
         return templateResolver;
     }
 
     @Bean
     public ServletContextTemplateResolver templateResolverServlet() {
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-        templateResolver.setPrefix("/WEB-INF/html/");
+        templateResolver.setPrefix("/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode("LEGACYHTML5");
         templateResolver.setOrder(2);
         templateResolver.setCacheable(false);
+
         return templateResolver;
     }
 
     @Bean
     public ViewResolver thymeleafViewResolver() {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-//        resolver.setViewClass(ThymeleafView.class);
+        // resolver.setViewClass(ThymeleafView.class);
         resolver.setTemplateEngine(templateEngine());
-        resolver.setOrder(1);
+        resolver.setOrder(3);
         resolver.setCharacterEncoding("UTF-8");
         resolver.setCache(false);
+
         return resolver;
     }
 
